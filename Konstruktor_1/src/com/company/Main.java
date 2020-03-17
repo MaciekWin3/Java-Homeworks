4c903b01b68ed5aaa220430e2f0a1e4dd4f5ac0c
MaciekWin3 <maciekwin3@gmail.com>
2020-03-17 10:40:21 +0100

    v2

--- Konstruktor_1/src/com/company/Main.java
+++ Konstruktor_1/src/com/company/Main.java
@@ -3,6 +3,29 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Przyklad Pierwszy = new Przyklad();
+        Przyklad Drugi = new Przyklad(12);
+        Pierwszy.wypisz();
+        Drugi.wypisz();
+    }
+}
+
+class Przyklad
+{
+    int pole;
+
+
+    public Przyklad() {
+        pole = 10;
+    }
+
+    public Przyklad(int a) {
+        pole = a;
+    }
+
+
+    void wypisz()
+    {
+        System.out.println("Pole wynosi : " + pole);
     }
 }
