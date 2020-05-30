2c89881d9ae86f4111bf9443e29ffe4e99599f43
MaciekWin3 <maciekwin3@gmail.com>
2020-05-30 13:13:12 +0200

    zaliczenie

---
+++ Zaliczenie-Zadanie-1/src/com/company/Main.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+    }
+}
+
+class Samochod{
+    int predkosc;
+    int dystans;
+
+
+    public void
+
+    public void przyspiesz(int predkosc) {
+        this.predkosc = predkosc;
+    }
+
+
+    public void wyswietlStan() {
+        System.out.println("Obecny stan");
+        System.out.println("Predkosc wynosi: " + predkosc);
+        System.out.println("Samochod pokonał dystans?");
+    }
+}
